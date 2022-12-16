a5e675c45a63f023df901b032ffe9a084056343b
twent <twentprod@gmail.com>
2022-12-17 00:40:26 +0300

    ✅ 1.2.1 Reactivity - Assignment

---
+++ src/routes/chapter-one/reactivity/assignments/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { HomeLink } from '$lib/index.js'
+
+	let count = 0
+
+	function increment() {
+		return count++
+	}
+</script>
+
+<button on:click={increment}>
+	Clicked {count}
+	{count === 1 ? 'time' : 'times'}
+</button>
+
+<HomeLink />
