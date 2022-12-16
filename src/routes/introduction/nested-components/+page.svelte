5a6937ef0eb6b14729fe5a7739c60cf7e7d26991
twent <twentprod@gmail.com>
2022-12-16 23:56:40 +0300

    ✅ 1.3, 1.4 Styles & nested components

---
+++ src/routes/introduction/nested-components/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { HomeLink } from '$lib/index.js'
+</script>
+
+<p>This is a paragraph.</p>
+<br />
+<HomeLink />
+
+<style>
+	p {
+		color: orange;
+		font-family: 'Comic Sans MS', cursive;
+		font-size: 2em;
+	}
+</style>
