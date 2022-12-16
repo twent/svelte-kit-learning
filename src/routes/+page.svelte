aef5c51060844e48ad6fbf6911da3827e7371342
twent <twentprod@gmail.com>
2022-12-17 00:32:42 +0300

    🛠️ Change structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,21 +4,32 @@
 
 <h1>SvelteKit Dev Learning Tasks:</h1>
 
-<h2>1. Introduction</h2>
+<h2>1 Getting Started with Svelte</h2>
+<h3>1.1 Introduction</h3>
 <ul>
 	<li>
-		<Link href="/introduction/first-component" title="Task 1 - First Component" />
+		<Link href="/chapter-one/introduction/first-component" title="Task 1 - First Component" />
 	</li>
 	<li>
-		<Link href="/introduction/dynamic-attributes" title="Task 2 - Dynamic Attributes" />
-	</li>
-	<li>
-		<Link href="/introduction/nested-components" title="Task 3, 4 - Styling & Nested Components" />
+		<Link href="/chapter-one/introduction/dynamic-attributes" title="Task 2 - Dynamic Attributes" />
 	</li>
 	<li>
 		<Link
-			href="/introduction/disable-html-sanitizer"
+			href="/chapter-one/introduction/nested-components"
+			title="Task 3, 4 - Styling & Nested Components"
+		/>
+	</li>
+	<li>
+		<Link
+			href="/chapter-one/introduction/disable-html-sanitizer"
 			title="Task 5 - Disable HTML Sanitizer (Dangerous!)"
 		/>
+	</li>
+</ul>
+
+<h3>1.2 Reactivity</h3>
+<ul>
+	<li>
+		<Link href="/chapter-one/reactivity/assignments" title="Task 1 - Assignments" />
 	</li>
 </ul>
